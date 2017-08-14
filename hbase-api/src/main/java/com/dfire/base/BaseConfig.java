9320ce213b85fe0fca21f47847edaf9c8e26f3cd
ranqiqiang <qiqiang@ran.gmail.com>
2017-08-14 15:20:28 +0800

    new conf

--- hbase-api/src/main/java/com/dfire/base/BaseConfig.java
+++ hbase-api/src/main/java/com/dfire/base/BaseConfig.java
@@ -8,12 +8,14 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
  * Created by qqr on 16/8/2.
  */
 public class BaseConfig {
-    public static final String ZK_ADDRESS = "zookeeper1.dihuo.online:2181,zookeeper2.dihuo.online:2181,zookeeper3.dihuo.online:2181";
+    public static final String ZK_ADDRESS = "zk1.2dfire-daily.com:2181,zk2.2dfire-daily.com:2181,zk3.2dfire-daily.com:2181";
     public static Configuration getConfiguration() {
         Configuration conf =  HBaseConfiguration.create();
-//        conf.set("hbase.rootdir", "hdfs://hadoop1:8020/hbase");
-//        conf.set("hbase.zookeeper.property.clientPort","2181");
+        conf.set("hbase.rootdir", "/hbase1");
+        conf.set("hbase.zookeeper.property.clientPort","2181");
         conf.set("hbase.zookeeper.quorum",ZK_ADDRESS);
+//         conf.set("hbase.master", "192.168.90.161:16000");
+        conf.set("zookeeper.znode.parent","/hbase1");
         return conf;
     }
 }
